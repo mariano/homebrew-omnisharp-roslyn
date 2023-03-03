b1aeeec9831f1ec42af11e4802107d1c75f371bb
Mariano Iglesias <mariano@cricava.com>
2023-03-03 12:18:33 -0300

    Upgrading to 1.39.4 for arm64

--- omnisharp.rb
+++ omnisharp.rb
@@ -1,9 +1,9 @@
 class Omnisharp < Formula
   desc "Cross platform .NET development in the editor of your choice"
   homepage "http://www.omnisharp.net/"
-  url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.35.3/omnisharp-osx.tar.gz"
+  url "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.39.4/omnisharp-osx-arm64-net6.0.tar.gz"
   version "1.35.3"
-  sha256 "b498337e4cc5293744d1e0df999ad56d03191fc83ba72be2b71f529ebebe0177"
+  sha256 "b8bb260d8c9295e55fcba7a408ae4d8ac0f5748d3868b61d4eda9af222d76c36"
   conflicts_with "omnisharp-mono"
 
   depends_on "mono" => :recommended
